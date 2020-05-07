143a8b8d43edb2196e0552d6014bf7795e35e125
RevolutionGo <32586385+RevolutionGo@users.noreply.github.com>
2020-05-07 19:22:18 +0800

    Update Constants.java

--- app/src/main/java/com/example01/myapplicationtest01/util/Constants.java
+++ app/src/main/java/com/example01/myapplicationtest01/util/Constants.java
@@ -3,10 +3,11 @@ package com.example01.myapplicationtest01.util;
 /**
  * 常量类
  * 用来保存接口地址以及一些常量
+ * 本地伪登录账号和密码
  */
 public class Constants {
-    public static final String USERNAME = "807318819@qq.com";
-    public static final String PASSWORD = "807318819bb";
+    public static final String USERNAME = "123456789@qq.com";
+    public static final String PASSWORD = "1234567890";
 
     public static final String ENDPOINT = "http://rap2api.taobao.org/app/mock/162466/";
     public static final String ID = "ID";
